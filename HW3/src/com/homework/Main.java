c9d06d73face0527c9566f6e248a2c3c51363f92
Mark Kalda <mark.kalda94@gmail.com>
2020-05-13 11:22:04 +0300

    methods names w/ underline

--- HW3/src/com/homework/Main.java
+++ HW3/src/com/homework/Main.java
@@ -9,9 +9,9 @@ public class Main {
     private static final String PATH_TO_FILE = "/Users/markkalda/Desktop/OOP/HW3/src/com/homework/%s.txt"; //change this with Yours path to file
 
     private static String data[] = new String[5];
-    private static int lines_count = 0;
+    private static int linesCount = 0;
     private static String city;
-    private static int shows_count = 0;
+    private static int showsCount = 0;
 
 
     public static void main(String[] args) throws IOException, FileNotFoundException {
@@ -19,43 +19,43 @@ public class Main {
 
         System.out.println("Hello, enter the file name: ");
 
-        String file_path = String.format(PATH_TO_FILE, reader.readLine());
+        String filePath = String.format(PATH_TO_FILE, reader.readLine());
 
-        read_from_file(file_path);
+        readFromFile(filePath);
 
     }
 
-    public static void count_input_file_lines(String path_to_file) throws IOException, FileNotFoundException {
+    public static void countInputFileLines(String path_to_file) throws IOException, FileNotFoundException {
         LineNumberReader reader = new LineNumberReader(new FileReader(path_to_file));
 
         while ((reader.readLine()) != null) {
-            lines_count++;
+            linesCount++;
         }
 
         reader.close();
     }
 
-    public static void read_from_file(String path_to_file) throws IOException, FileNotFoundException {
+    public static void readFromFile(String path_to_file) throws IOException, FileNotFoundException {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        count_input_file_lines(path_to_file);
+        countInputFileLines(path_to_file);
 
         Scanner sc = new Scanner(new FileReader(path_to_file));
 
-        process_data(sc.nextLine());
+        processData(sc.nextLine());
 
-        show_data(data, city);
+        showData(data, city);
 
-        shows_count++;
+        showsCount++;
 
-        for (int i = shows_count; shows_count < lines_count; i++) {
+        for (int i = showsCount; showsCount < linesCount; i++) {
             System.out.println("There are more data to show. Do You want to see it? y/n");
             if (reader.readLine().equals("y")) {
-                process_data(sc.nextLine());
-                show_data(data, city);
-                shows_count++;
-                if (shows_count == lines_count)
+                processData(sc.nextLine());
+                showData(data, city);
+                showsCount++;
+                if (showsCount == linesCount)
                     System.out.println("There are no more data in this file");
             } else {
 
@@ -68,23 +68,23 @@ public class Main {
 
     }
 
-    public static void process_data(String full_line) {
+    public static void processData(String fullLine) {
         int index_of_first_digit = 0;
 
-        for (char c : full_line.toCharArray()) {
+        for (char c : fullLine.toCharArray()) {
             if (Character.isDigit(c)) {
-                index_of_first_digit = full_line.indexOf(c);
+                index_of_first_digit = fullLine.indexOf(c);
                 break;
             }
         }
 
-        city = full_line.substring(0, index_of_first_digit - 1);
+        city = fullLine.substring(0, index_of_first_digit - 1);
 
-        data = full_line.substring(index_of_first_digit).split(" ");
+        data = fullLine.substring(index_of_first_digit).split(" ");
     }
 
 
-    public static int coldest_temp(String[] data) {
+    public static int coldestTemp(String[] data) {
         int minimum = Integer.parseInt(data[0]);
 
         for (int i = 0; i < data.length; i++) {
@@ -96,7 +96,7 @@ public class Main {
         return minimum;
     }
 
-    public static int warmest_temp(String[] data) {
+    public static int warmestTemp(String[] data) {
         int maximum = Integer.parseInt(data[0]);
 
         for (int i = 0; i < data.length; i++) {
@@ -108,41 +108,41 @@ public class Main {
         return maximum;
     }
 
-    public static double average_temp(String[] data) {
-        double sum_of_temps = 0;
+    public static double averageTemp(String[] data) {
+        double sumOfTemps = 0;
 
 
         for (int i = 0; i < data.length; i++) {
-            sum_of_temps += Double.parseDouble(data[i]);
+            sumOfTemps += Double.parseDouble(data[i]);
 
         }
 
-        double result = sum_of_temps / data.length;
+        double result = sumOfTemps / data.length;
 
         return result;
     }
 
 
-    public static double average_temp_without_extremes(String[] data) {
-        double sum_of_temps = 0;
+    public static double averageTempWithoutExtremes(String[] data) {
+        double sumOfTemps = 0;
 
         for (int i = 0; i < data.length; i++) {
-            if (Double.parseDouble(data[i]) == warmest_temp(data) || Double.parseDouble(data[i]) == coldest_temp(data)) {
+            if (Double.parseDouble(data[i]) == warmestTemp(data) || Double.parseDouble(data[i]) == coldestTemp(data)) {
                 continue;
             } else {
-                sum_of_temps += Double.parseDouble(data[i]);
+                sumOfTemps += Double.parseDouble(data[i]);
             }
         }
 
-        double result = sum_of_temps / (data.length - 2);
+        double result = sumOfTemps / (data.length - 2);
 
         return result;
     }
 
-    public static void show_data(String[] data, String city) {
-        System.out.println(String.format("Coldest temperature for %s was %d", city, coldest_temp(data)));
-        System.out.println(String.format("Warmest temperature for %s was %d", city, warmest_temp(data)));
-        System.out.println(String.format("Average temperature for %s was %.1f", city, average_temp(data)));
-        System.out.println(String.format("Average temperature without extremes for %s was %.1f", city, average_temp_without_extremes(data)));
+    public static void showData(String[] data, String city) {
+        System.out.println(String.format("Coldest temperature for %s was %d", city, coldestTemp(data)));
+        System.out.println(String.format("Warmest temperature for %s was %d", city, warmestTemp(data)));
+        System.out.println(String.format("Average temperature for %s was %.1f", city, averageTemp(data)));
+        System.out.println(String.format("Average temperature without extremes for %s was %.1f", city, averageTempWithoutExtremes(data)));
     }
 }
